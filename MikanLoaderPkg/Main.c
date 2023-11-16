7bde705d4495110f9451aedc635c66742633de85
Yuki Shimada <emadurandal@gmail.com>
2023-11-16 11:36:34 +0900

    コメント追加
    # Conflicts:
    #	MikanLoaderPkg/Main.c

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -214,36 +214,64 @@ EFI_STATUS EFIAPI UefiMain(
   }
   // #@@range_end(gop)
 
+  // #@@range_begin(read_kernel)
+  // kernel.elfを開く
   EFI_FILE_PROTOCOL* kernel_file;
   root_dir->Open(
       root_dir, &kernel_file, L"\\kernel.elf",
       EFI_FILE_MODE_READ, 0);
 
+  // 開いたkernel.elfファイル全体を読み込むには、そのファイルサイズを知る必要がある。
+  // そのためファイルサイズを取得するためkernel_file->GetInfo()を呼び出す。
+  // その情報の受け皿となるfile_info_bufferを用意する。
+  // sizeof(CHAR16) * 12はファイル名の文字数分のバッファを確保するためのもので、
+  // 詳しくは書籍p75を参照。
   UINTN file_info_size = sizeof(EFI_FILE_INFO) + sizeof(CHAR16) * 12;
   UINT8 file_info_buffer[file_info_size];
+
+  // kernel_file->GetInfo()を呼び出してファイルサイズを取得する。
   kernel_file->GetInfo(
       kernel_file, &gEfiFileInfoGuid,
       &file_info_size, file_info_buffer);
 
+  // file_info_bufferはUINT8型なので、EFI_FILE_INFO*型にキャストする。
   EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
+  // ファイルサイズはfile_info->FileSizeに格納されている。
   UINTN kernel_file_size = file_info->FileSize;
 
+
+  // kernel.elfを読み込むためのメモリを確保する。
+  // ld.lldのオプション--image-baseで指定したアドレスをkernel_base_addrに設定する。
+  // カーネルファイルはこの番地に配置して動作させる前提で作ってあるということ。
   EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
   gBS->AllocatePages(
-      AllocateAddress, EfiLoaderData,
-      (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+      AllocateAddress, EfiLoaderData, // AllocateAddressは指定したアドレスにメモリ確保せよという意味、EfiLoaderDataは確保するメモリ領域の種別でブートローダーが使うための領域なら通常はこれを指定すれば良い
+      (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr); // (kernel_file_size + 0xfff) / 0x1000はカーネルファイルサイズをバイト単位からページ単位に変換する計算式。詳しくは書籍p77を参照。
+
+  // kernel.elfをkernel_base_addrに読み込む。  
   kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
+  // 画面にデバッグ出力
   Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
 
+
+  // #@@range_end(read_kernel)
+
   // #@@range_begin(exit_bs)
+  // OSの邪魔にならないように、今まで動いていたUEFI BIOSのブートサービスを停止する
+  // この後はブートサービスの機能（Print()やファイルやメモリ関連の機能）は使えなくなる
+  // この関数はその時点での最新のメモリマップのマップキーを要求する（第２引数）
+  // gBS->ExitBootServices()は指定されたマップキーが最近のメモリマップに紐づくマップキーでない場合、実行に失敗する
+  // 色々ブートろサービスの機能を使っているので、おそらく一回目は失敗する
   EFI_STATUS status;
   status = gBS->ExitBootServices(image_handle, memmap.map_key);
   if (EFI_ERROR(status)) {
+    // 失敗した場合は、再度メモリマップを所得する
     status = GetMemoryMap(&memmap);
     if (EFI_ERROR(status)) {
       Print(L"failed to get memory map: %r\n", status);
       while (1);
     }
+    // 更新したマップキーで再度ブートサービスの停止を試みる。これは成功するはず。
     status = gBS->ExitBootServices(image_handle, memmap.map_key);
     if (EFI_ERROR(status)) {
       Print(L"Could not exit boot service: %r\n", status);
@@ -253,10 +281,15 @@ EFI_STATUS EFIAPI UefiMain(
   // #@@range_end(exit_bs)
 
   // #@@range_begin(call_kernel)
+  // カーネルのエントリポイントアドレスを取得する
   UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
 
+  // エントリポイントのC言語としての関数プロトタイプ情報を定義する
   typedef void EntryPointType(void);
+  // エントリポイントのアドレスを関数ポインタとしてキャスト
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
+
+  // カーネルのエントリポイントを呼び出す
   entry_point();
   // #@@range_end(call_kernel)
 
