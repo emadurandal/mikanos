2e22cf834062c6db7d56e8112301b376872126f8
Yuki Shimada <emadurandal@gmail.com>
2023-11-22 22:07:58 +0900

    コメント追加

--- kernel/console.cpp
+++ kernel/console.cpp
@@ -35,18 +35,22 @@ void Console::PutString(const char* s) {
 // #@@range_begin(newline)
 void Console::Newline() {
   cursor_column_ = 0;
-  if (cursor_row_ < kRows - 1) {
-    ++cursor_row_;
+  if (cursor_row_ < kRows - 1) { // 現在のカーソル位置がまだ最下行に達していないなら
+    ++cursor_row_; // カーソルを一行下に移動させる
   } else {
+    // 表示領域全域を塗りつぶす
     for (int y = 0; y < 16 * kRows; ++y) {
       for (int x = 0; x < 8 * kColumns; ++x) {
         writer_.Write(x, y, bg_color_);
       }
     }
     for (int row = 0; row < kRows - 1; ++row) {
+      // row + 1行目の文字列をrow行目にコピーする
       memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
+      // row行目を画面に描画する
       WriteString(writer_, 0, 16 * row, buffer_[row], fg_color_);
     }
+    // 最下行をNULL文字で初期化する
     memset(buffer_[kRows - 1], 0, kColumns + 1);
   }
 }
